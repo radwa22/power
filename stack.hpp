25058596196c90cbed6ae86ab4a8da73cee90d1f
Radwa Mostafa <eng.radwa2019@gmail.com>
2018-03-22 23:17:26 -0400

    queue

---
+++ stack.hpp
@@ -0,0 +1,58 @@
+
+namespace RH
+{
+
+struct IntegersStack
+{
+  int top = -1 ;
+  int buffer[1000];
+};
+void push( IntegersStack &stack , int data )
+{  
+                  
+    stack.top ++ ;
+   stack.buffer[stack.top] = data ;
+}
+int pop( IntegersStack &stack )
+{
+    stack.top-- ;
+    return stack.buffer[stack.top];
+    
+}
+
+int front( IntegersStack &stack )
+{
+
+  return stack.buffer [stack.top];
+
+}
+bool isEmpty( IntegersStack &stack )
+{
+
+   return (stack.top == -1);
+
+}
+int size( IntegersStack &stack )
+{
+
+    return stack.top + 1 ;
+}
+void clear( IntegersStack &stack )
+{
+  stack.buffer[-1];
+
+}
+//using linked list
+struct Node
+{
+  int data;
+    Node* next = nullptr;
+};
+
+
+struct stackll
+ {
+    Node *front = nullptr;
+ };
+
+}
